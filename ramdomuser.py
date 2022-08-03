9475abc22c95308dc0feab01e43fc37050f493a3
Raul Deus <jd.raul@gmail.com>
2022-08-02 21:42:07 -0300

    Teste

--- ramdomuser.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from randomuser import RandomUser
-
-# Gera uma lista com 5000 usuarios do Brasil
-user_list = RandomUser.generate_users(10, {'nat': 'br'})
-
---
+++ randomuser.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from randomuser import RandomUser
+
+# Generate a list of 10 random users
+user_list = RandomUser.generate_users(10, {'nat': 'ca'})
