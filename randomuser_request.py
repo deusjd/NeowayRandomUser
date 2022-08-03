053038896e7ae5460a108c0bb1d1569692a8d803
Raul Deus <jd.raul@gmail.com>
2022-08-03 04:24:10 -0300

    Inserindo e normalizando os dados

--- randomuser_request.py
+++ randomuser_request.py
@@ -8,11 +8,63 @@ from config import config
 def create_tables():
     commands = (
         """
-        CREATE TABLE IF NOT EXISTS vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """,
+            create table pais (
+            id serial primary key,
+            nome varchar
+            )
+            """,
+            """
+            create table cidade (
+            id serial primary key,
+            pais_id integer,
+            nome varchar,
+            foreign key (pais_id) references pais (id)
+            )
+            """,
+            """
+            create table tipo_logradouro (
+            id serial primary key,
+            nome varchar
+            )
+            """,
+            """
+            create table logradouro (
+            id serial primary key,
+            cidade_id integer,
+            tipo_logradouro_id integer,
+            nome varchar,
+            cep varchar,
+            latitude decimal(11,8),
+            longitude decimal(11,8),
+            foreign key (cidade_id) references cidade (id),
+            foreign key (tipo_logradouro_id) references tipo_logradouro (id)
+            )
+            """,
+            """
+            create table usuario (
+            id serial primary key,
+            logradouro_id integer,
+            cpf varchar,
+            nome varchar,
+            sbrenome varchar,
+            sexo varchar,
+            data_nascimento timestamp,
+            email varchar,
+            telefone varchar,
+            celular varchar,
+            naturalidade varchar,
+            logradouro_numero integer,
+            data_cadastro timestamp,
+            endereco_completo varchar,
+            idade int,
+            classificacao_etaria varchar generated always as (case when idade < 12 then 'CRIANÇA'
+                                                                when idade between 12 and 18 then 'ADOLESCENTE'
+                                                                when idade > 18 then 'ADULTO' END) stored,
+            finalidade_uso varchar generated always as (case when idade > 18 then 'PRODUTOS DE MARKETING'
+                                                                else 'PRODUTOS DE RISCO' END) stored,
+            foreign key (logradouro_id) references logradouro (id)
+            )
+            """,
         """ CREATE TABLE IF NOT EXISTS USERS (
                 users_id serial primary key, 
                 gender varchar,
@@ -54,7 +106,7 @@ def create_tables():
 
 def insert_users():
 
-    sql_inset = """INSERT INTO users(gender,
+    sql_insert = """INSERT INTO users(gender,
                                     name_first,
                                     name_last,
                                     dob_date,
@@ -90,7 +142,7 @@ def insert_users():
                 params = config()
                 conn = psycopg2.connect(**params)
                 cur = conn.cursor()
-                cur.execute(sql_inset, [(i['gender'],),
+                cur.execute(sql_insert, [(i['gender'],),
                                         (i['name']['first'],),
                                         (i['name']['last'],),
                                         (i['dob']['date'],),
@@ -117,6 +169,74 @@ def insert_users():
                 if conn is not None:
                     conn.close()
 
+def normalize_data():
+    commands = ("""
+            insert into pais (nome)  
+            select distinct trim(location_country)  
+              from users
+              """
+            , """
+            insert into cidade (nome, pais_id)  
+            select distinct trim(location_city) , p.id
+              from users u
+              join pais p on u.location_country = p.nome ;
+              """
+            , """
+             insert into tipo_logradouro (nome)
+             select distinct trim(split_part(location_street_name,' ',1)) from users 
+              """
+            , """
+             insert into logradouro (cidade_id, tipo_logradouro_id, nome, cep, latitude, longitude)
+             select distinct c.id 
+                           , tl.id
+                           , trim(replace(u.location_street_name,trim(split_part(u.location_street_name,' ',1)),''))
+                           , u.location_postcode
+                           , u.location_coordinates_latitude 
+                           , u.location_coordinates_longitude  
+               from users u 
+               left join cidade c on u.location_city = c.nome 
+               left join tipo_logradouro tl on trim(split_part(u.location_street_name,' ',1)) = tl.nome 
+              """
+            , """
+            insert into usuario (logradouro_id, cpf, nome, sbrenome, sexo, data_nascimento, email, telefone, celular, naturalidade, logradouro_numero, data_cadastro, endereco_completo, idade)
+            select l.id
+                , u.id_value
+                , u.name_first 
+                , u.name_last 
+                , case u.gender when 'female' then 'FEMININO'
+                            when 'male'	 then 'MASCULINO' end
+            , u.dob_date 
+            , email 
+            , phone 
+            , cell 
+            , nat 
+            , location_street_number 
+            , now() 
+            , concat(u.location_street_name,' ', u.location_street_number, ', ', u.location_postcode, ', ', u.location_city, ', ', u.location_country)
+            , date_part('year',now())-date_part('year',u.dob_date)
+            from users u 
+            left join logradouro l on trim(replace(u.location_street_name,trim(split_part(u.location_street_name,' ',1)),'')) = l.nome 
+                                and u.location_postcode = l.cep
+                                and u.location_coordinates_latitude  = l.latitude 
+                                and location_coordinates_longitude = l.longitude 
+            left join tipo_logradouro tl on l.tipo_logradouro_id = tl.id
+                left join cidade c on l.cidade_id = c.id 
+            """)
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        for command in commands:
+            cur.execute(command)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 if __name__ == '__main__':
     create_tables()
     insert_users()
+    normalize_data()
